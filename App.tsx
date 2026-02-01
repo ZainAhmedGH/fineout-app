e642b7ddaf335545c15251d3801b5feba270f77a
ZainAhmedGH <zainzee19@gmail.com>
2026-02-01 17:41:55 +0000

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,30 @@
+
+import React, { useState } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ServicePage from './pages/ServicePage';
+import ContactPage from './pages/ContactPage';
+import Layout from './components/Layout';
+import { AdBumper } from './components/AdBumper';
+
+const App: React.FC = () => {
+  const [hasEntered, setHasEntered] = useState(false);
+
+  if (!hasEntered) {
+    return <AdBumper onComplete={() => setHasEntered(true)} />;
+  }
+
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/choose-your-service" element={<ServicePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
+};
+
+export default App;
